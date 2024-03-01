83d73a695c15ef36b56581a4f3f7e48fb2cd40f5
Jjianuo <Jwen@ucm.es>
2024-03-01 18:30:42 +0100

    Actualizado include

--- src/BilliardsAdrift/BilliardsAdrift.cpp
+++ src/BilliardsAdrift/BilliardsAdrift.cpp
@@ -1,10 +1,10 @@
 #include "BilliardsAdrift.h"
-#include "Structure/ComponentFactory.h"
+#include "Structure/ComponentBuilder.h"
 #include "defs.h"
 
 JUEGO_API void function(const char* var)
 {
-    std::cout << "I love " << var << "<3\n";
+    std::cout << "I love BilliardsAdrift" << var << "<3\n";
 }
 
 JUEGO_API FactoryInfo** getComponentFactories(int& count)
