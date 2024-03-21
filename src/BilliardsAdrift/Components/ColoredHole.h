4e0bcf075b14314986145092e0a3e52a558dca3c
Santienper <stienda@ucm.es>
2024-03-21 10:50:08 +0100

    Pequeño ajuste de ColoredHole

---
+++ src/BilliardsAdrift/Components/ColoredHole.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Structure/Component.h>
+#include "../gameDefs.h"
+
+class JUEGO_API ColoredHole : public Tapioca::Component {
+private:
+
+public:
+    COMPONENT_ID("ColoredHole");
+
+    ColoredHole() = default;
+
+};
