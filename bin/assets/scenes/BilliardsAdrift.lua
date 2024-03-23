b8c53841d8798b8e4dac9b3b49963489227d0794
NanxiQin <nanxiqin@ucm.es>
2024-03-23 14:47:56 +0100

    CueController Base+error ui

--- bin/assets/scenes/BilliardsAdrift.lua
+++ bin/assets/scenes/BilliardsAdrift.lua
@@ -32,12 +32,12 @@ scene = {
             }
         }
     },
-    Cue={
+    CueContainer={
         components={
             Transform = {
                 positionX = 0.0,
                 positionY = 0.0,
-                positionZ = 0.0,
+                positionZ = 0.0,--offset
                 scaleX = 10.0,
                 scaleY = 10.0,
                 scaleZ = 10.0,
@@ -54,18 +54,71 @@ scene = {
                 colliderScaleY=0.3,
                 colliderScaleZ=5.0,
                 isTrigger=false,
-                movementType=0, --0 DYNAMIC , 1 STATIC, 2 KINEMATIC
+                movementType=2, --0 DYNAMIC , 1 STATIC, 2 KINEMATIC
                 mass=10.0,
                 friction=1.0,
                 damping=0.4,
                 bounciness=0.0
             },
             CueController = {    
-                powerFactor=20.0,
+                powerFactor=3.0,
                 moveFactor=1.0
             }
         }
     },
+ 
+    -- Cue={
+    --     components={
+    --         Transform = {
+    --             positionX = 0.0,
+    --             positionY = 0.0,
+    --             positionZ = 0.0,
+    --             scaleX = 1.0,
+    --             scaleY = 1.0,
+    --             scaleZ = 1.0,
+    --             rotationX = 0.0,
+    --             rotationY = 0.0,
+    --             rotationZ = 0.0
+    --         } 
+    --      },
+    --     children ={
+    --     CueContainer={
+    --         components={
+    --             Transform = {
+    --                 positionX = 0.0,
+    --                 positionY = 0.0,
+    --                 positionZ = 0.0,--offset
+    --                 scaleX = 10.0,
+    --                 scaleY = 10.0,
+    --                 scaleZ = 10.0,
+    --                 rotationX = 0.0,
+    --                 rotationY = 30.0,
+    --                 rotationZ = 0.0
+    --             }, 
+    --             MeshRenderer = {
+    --                 meshName = "CueStick/CueStick.mesh",
+    --             },
+    --             RigidBody = {    
+    --                 colShape=0, --0 BOX, 1 SPHERE, 2 PLANE, 3 CAPSULE
+    --                 colliderScaleX=0.3,
+    --                 colliderScaleY=0.3,
+    --                 colliderScaleZ=5.0,
+    --                 isTrigger=false,
+    --                 movementType=2, --0 DYNAMIC , 1 STATIC, 2 KINEMATIC
+    --                 mass=10.0,
+    --                 friction=1.0,
+    --                 damping=0.4,
+    --                 bounciness=0.0
+    --             },
+    --             CueController = {    
+    --                 powerFactor=3.0,
+    --                 moveFactor=1.0
+    --             }
+    --         }
+    --     }
+    --     }
+    -- },
+   
     Ground={
         components={
             Transform = {
