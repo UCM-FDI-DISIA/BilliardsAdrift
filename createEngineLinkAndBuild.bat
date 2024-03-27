18cab82563b4d8b9b62f187ef741d0f63ab5def1
MattCastUCM <mattcast@ucm.es>
2024-03-27 22:17:04 +0100

    Script para crear enlace al motor

---
+++ createEngineLinkAndBuild.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Este archivo deberia ir a la misma altura que el .sln de los juegos
+
+set BAT_DIR=%~dp0
+set PARENT_DIR=%BAT_DIR%..\
+
+set LNK_NAME=TapiocaEngine
+set LNK_DIR=%BAT_DIR%%LNK_NAME%\
+
+echo %PARENT_DIR%%LNK_NAME%
+
+if exist %LNK_DIR% (
+    echo %LNK_DIR% ya existe, eliminando
+    rmdir %LNK_DIR%
+)
+mklink /D %LNK_NAME% %PARENT_DIR%%LNK_NAME%
+if errorlevel 1 (
+    echo Error al crear enlace con el motor
+) else (
+    echo Enlace con el motor creado correctamente
+)
+
+call %LNK_DIR%buildEngine.bat
+
+pause
