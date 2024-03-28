6fb23e9eceb6c1a8e85db0e62967172252cce728
MattCastUCM <mattcast@ucm.es>
2024-03-28 19:32:21 +0100

    Update createEngineLinkAndBuild.bat

--- createEngineLinkAndBuild.bat
+++ createEngineLinkAndBuild.bat
@@ -14,13 +14,15 @@ if exist %LNK_DIR% (
     echo %LNK_DIR% ya existe, eliminando
     rmdir %LNK_DIR%
 )
-mklink /d %BAT_DIR%%LNK_NAME% %PARENT_DIR%%LNK_NAME%
+echo Creando enlace a %LNK_NAME% en %LNK_DIR%
+
+mklink /D %BAT_DIR%%LNK_NAME% %PARENT_DIR%%LNK_NAME%
 if errorlevel 1 (
     echo Error al crear enlace con el motor
 ) else (
     echo Enlace con el motor creado correctamente
+    call %LNK_DIR%buildEngine.bat
 )
 
-call %LNK_DIR%buildEngine.bat
 
 pause
