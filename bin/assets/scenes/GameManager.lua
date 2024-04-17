fbb73317cdedfdefa9be452a38efb2283c0ddc31
Laura Wang Qiu <yiwang03@ucm.es>
2024-04-18 01:42:36 +0200

    Limpieza y cambio de startScene a StartScene

--- bin/assets/scenes/GameManager.lua
+++ bin/assets/scenes/GameManager.lua
@@ -1,17 +1,6 @@
 scene = {
     {
         components = {
-            Transform = {
-                positionX = 0.0,
-                positionY = 0.0,
-                positionZ = 0.0,
-                scaleX = 1.0,
-                scaleY = 1.0,
-                scaleZ = 1.0,
-                rotationX = 0.0,
-                rotationY = 0.0,
-                rotationZ = 0.0
-            },
             GameManager = {
                 firstStateName = "Level1", -- Cambiar a MainMenu cuando se gestione los callbacks de los botones
                 initLife = 3,
