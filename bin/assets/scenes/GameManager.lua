a6dd1c9e7d1bc6ab03cc2010ed4d50948fc4e941
pedroleo <pedroleo@ucm.es>
2024-04-20 04:08:47 +0200

    Camara

--- bin/assets/scenes/GameManager.lua
+++ bin/assets/scenes/GameManager.lua
@@ -7,5 +7,29 @@ scene = {
                 initTime = 9999.0
             }
         }
-    }
+    },
+    {
+        components = {
+            Transform = {
+                positionX = 0.0,
+                positionY = 60.0,
+                positionZ = 0.0,
+                scaleX = 1.0,
+                scaleY = 1.0,
+                scaleZ = 1.0,
+                rotationX = 0.0,
+                rotationY = 0.0,
+                rotationZ = 90.0
+            },
+            CameraComponent = {
+                zOrder = 0,
+                bgColorR = 0.925,
+                bgColorG = 0.698,
+                bgColorB = 0.941,
+                targetToLookX = 0.0,
+                targetToLookY = 0.0,
+                targetToLookZ = 0.0
+            }
+        }
+    },
 }
