5e8860fed46975ef7084f03dbd5b44b4ba7d097a
MattCastUCM <mattcast@ucm.es>
2024-05-10 01:46:43 +0200

    Movable wall usa bien el deltatime

--- src/BilliardsAdrift/Components/MovableWall.cpp
+++ src/BilliardsAdrift/Components/MovableWall.cpp
@@ -40,9 +40,8 @@ void MovableWall::start() {
 void MovableWall::update(const uint64_t deltaTime) {
     Tapioca::Vector3 v = tr->getGlobalPosition();
     if (v.x < origin.x - 0.5f || v.z < origin.y - 0.5f) direction = dest - origin;
-    else if (v.x > dest.x + 0.5f || v.z > dest.y + 0.5f)
-        direction = origin - dest;
+    else if (v.x > dest.x + 0.5f || v.z > dest.y + 0.5f) direction = origin - dest;
 
     Tapioca::Vector2 auxD = direction.getNormalized();
-    tr->translate(Tapioca::Vector3(auxD.x, 0, auxD.y) * speed * Tapioca::MainLoop::FIXED_DELTA_TIME);
+    tr->translate(Tapioca::Vector3(auxD.x, 0, auxD.y) * speed * deltaTime);
 }
